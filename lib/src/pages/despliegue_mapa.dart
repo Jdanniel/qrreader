404eae0deb948214c93bf4ac3d08e43bed5f0f5d
Daniel Ojeda <ing.danniel@outlook.com>
2019-08-19 18:08:17 -0500

    Se agrega floating action Button para cambiar entre mapas

--- lib/src/pages/despliegue_mapa.dart
+++ lib/src/pages/despliegue_mapa.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreader/src/models/scan_model.dart';
 
-class DespliegueMapaPage extends StatelessWidget {
+class DespliegueMapaPage extends StatefulWidget {
 
+  @override
+  _DespliegueMapaPageState createState() => _DespliegueMapaPageState();
+}
+
+class _DespliegueMapaPageState extends State<DespliegueMapaPage> {
   final MapController map = new MapController();
+
+  String tipoMapa = 'streets';
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +31,28 @@ class DespliegueMapaPage extends StatelessWidget {
         ],
       ),
       body: _crearFlutterMap(scan),
+      floatingActionButton: _crearBotonFlotante(context),
+    );
+  }
+
+  Widget _crearBotonFlotante(BuildContext context){
+    return FloatingActionButton(
+      child: Icon(Icons.repeat,),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: (){
+        if(tipoMapa == 'streets'){
+          tipoMapa = 'dark';
+        }else if(tipoMapa == 'dark'){
+          tipoMapa = 'light';
+        }else if(tipoMapa == 'light'){
+          tipoMapa = 'outdoors';
+        }else if(tipoMapa == 'outdoors'){
+          tipoMapa = 'satellite';
+        }else{
+          tipoMapa = 'streets';
+        }
+        setState(() {});
+      },
     );
   }
 
@@ -48,7 +77,7 @@ class DespliegueMapaPage extends StatelessWidget {
       '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
       additionalOptions: {
         'accessToken' : 'pk.eyJ1IjoiamRhbm5pZWwiLCJhIjoiY2p6aXlhOWUyMDRjeTNubWl1bzE1ZTluZCJ9.b5OwP4lccT5Ebqoc0SD07Q',
-        'id' : 'mapbox.streets' //streets, dark, light, outdoors, satellite
+        'id' : 'mapbox.$tipoMapa' //streets, dark, light, outdoors, satellite
       }
     );
 
@@ -68,5 +97,4 @@ class DespliegueMapaPage extends StatelessWidget {
       ]
     );
   }
-
 }
